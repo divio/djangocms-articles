737981e95e5551ffa84ece3ac6bd1bea732ec20b
Angelo Dini <angelo.dini@divio.ch>
2019-02-21 12:18:20 +0100

    fix tests

--- djangocms_articles/cms_plugins.py
+++ djangocms_articles/cms_plugins.py
@@ -5,21 +5,11 @@ from cms.plugin_pool import plugin_pool
 from . import models
 
 
-class SocialPlugin(CMSPluginBase):
-    model = models.Social
-    name = 'Social Plugin'
-    render_template = 'my_custom_social_addon/social.html'
-    allow_children = True
-    child_classes = ['SocialIconPlugin']
+# class ...(CMSPluginBase):
+    # model = models.YourModel
+    # name = 'YourModel Plugin'
+    # render_template = 'templates/plugin.html'
+    # allow_children = True
 
 
-class SocialIconPlugin(CMSPluginBase):
-    model = models.SocialIcon
-    name = 'Icon'
-    render_template = 'my_custom_social_addon/icon.html'
-    require_parent = True
-    parent_classes = ['SocialPlugin']
-
-
-plugin_pool.register_plugin(SocialPlugin)
-plugin_pool.register_plugin(SocialIconPlugin)
+# plugin_pool.register_plugin(...)
